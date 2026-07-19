09084e5869eff1bee3a072ef63a05b4cd6268641
Evan Reynolds <919058@g.dentonisd.org>
2026-07-18 23:45:12 -0700

    Delete base44Client.js

--- base44Client.js
+++
@@ -1,7 +0,0 @@
-echo "# ddb" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/919058/ddb.git
-git push -u origin main
